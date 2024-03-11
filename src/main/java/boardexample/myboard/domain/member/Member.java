76d591a7042c7ab2657b6ef1754d8f3421b1b94b
fewolee <chulwoo25@naver.com>
2024-03-11 16:13:41 +0900

    JWT 관련 서비스 구현

--- src/main/java/boardexample/myboard/domain/member/Member.java
+++ src/main/java/boardexample/myboard/domain/member/Member.java
@@ -35,6 +35,9 @@ public class Member extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     private Role role;//권한 -> USER, ADMIN
 
+    @Column(length = 1000)
+    private String refreshToken;//RefreshToken
+
 
     //== 정보 수정 ==//
     public void updatePassword(PasswordEncoder passwordEncoder, String password){
@@ -56,6 +59,13 @@ public class Member extends BaseTimeEntity {
     //== 패스워드 암호화 ==//
     public void encodePassword(PasswordEncoder passwordEncoder){
         this.password = passwordEncoder.encode(password);
+    }
+
+    public void updateRefreshToken(String refreshToken){
+        this.refreshToken = refreshToken;
+    }
+    public void destroyRefreshToken(){
+        this.refreshToken = null;
     }
 
 }
