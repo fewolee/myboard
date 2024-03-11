bfc03f62599ee97a55155533840f8796320ad7f3
fewolee <chulwoo25@naver.com>
2024-03-11 15:38:18 +0900

    시큐리티 로그인 Json 커스텀 구현

--- src/main/java/boardexample/myboard/global/config/SecurityConfig.java
+++ src/main/java/boardexample/myboard/global/config/SecurityConfig.java
@@ -1,20 +1,30 @@
 package boardexample.myboard.global.config;
+import boardexample.myboard.domain.member.handler.LoginFailureHandler;
+import boardexample.myboard.domain.member.handler.LoginSuccessJWTProvideHandler;
+import boardexample.myboard.domain.member.service.LoginService;
+import boardexample.myboard.global.login.filter.JsonUsernamePasswordAuthenticationFilter;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-
+    private final ObjectMapper objectMapper;
+    private final LoginService loginService;
 
 
     @Bean
@@ -40,6 +50,7 @@ public class SecurityConfig {
                         .requestMatchers("/login", "/signup,","").permitAll() // 로그인, 회원가입, 메인페이지 허용
                         .anyRequest().authenticated());
 
+        http.addFilterAfter(jsonUsernamePasswordLoginFilter(), LogoutFilter.class);
 
         return http.build();
 
@@ -50,4 +61,30 @@ public class SecurityConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager() {//2 - AuthenticationManager 등록
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();//DaoAuthenticationProvider 사용
+        provider.setPasswordEncoder(passwordEncoder());//PasswordEncoder로는 PasswordEncoderFactories.createDelegatingPasswordEncoder() 사용
+        provider.setUserDetailsService(loginService);
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public LoginSuccessJWTProvideHandler loginSuccessJWTProvideHandler(){
+        return new LoginSuccessJWTProvideHandler();
+    }
+
+    @Bean
+    public LoginFailureHandler loginFailureHandler(){
+        return new LoginFailureHandler();
+    }
+
+    @Bean
+    public JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordLoginFilter(){
+        JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordLoginFilter = new JsonUsernamePasswordAuthenticationFilter(objectMapper);
+        jsonUsernamePasswordLoginFilter.setAuthenticationManager(authenticationManager());
+        //jsonUsernamePasswordLoginFilter.setAuthenticationSuccessHandler(loginSuccessJWTProvideHandler());
+        //jsonUsernamePasswordLoginFilter.setAuthenticationFailureHandler(loginFailureHandler());
+        return jsonUsernamePasswordLoginFilter;
+    }
 }
